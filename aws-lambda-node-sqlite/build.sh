85667845f0b30d14fd947f7a55b9f42f9df55cc5
Osman <1279406+pegasuspect@users.noreply.github.com>
2020-08-24 17:30:46 -0400

    build fix

--- aws-lambda-node-sqlite/build.sh
+++ aws-lambda-node-sqlite/build.sh
@@ -1,2 +1,2 @@
-rm -f function.zip;
-zip -rq function.zip . && echo 'Build into funciton.zip' || echo 'Build failed.'
+rm -f function.zip node_modules package-lock.json;
+npm i && zip -rq function.zip index.js node_modeules && echo 'Build into funciton.zip' || echo 'Build failed.'
