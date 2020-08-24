44f44054a59ce2aadf0425d4bb741b8fbb9ec5a9
Osman <1279406+pegasuspect@users.noreply.github.com>
2020-08-24 17:35:30 -0400

    typo

--- aws-lambda-node-sqlite/build.sh
+++ aws-lambda-node-sqlite/build.sh
@@ -1,2 +1,2 @@
 rm -f function.zip node_modules package-lock.json;
-npm i && zip -rq function.zip index.js node_modeules && echo 'Build into funciton.zip' || echo 'Build failed.'
+npm i && zip -rq function.zip index.js node_modules && echo 'Build into funciton.zip' || echo 'Build failed.'
